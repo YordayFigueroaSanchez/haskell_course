a9f9bee40cd77997a6720d10ee3599aa4183ad36
yfsanchez <yfsanchez@gmail.com>
2021-11-13 18:01:55 -0300

    Implementing zipWith
    Implementing zipWith

--- chapter5.hs
+++ chapter5.hs
@@ -12,3 +12,9 @@ divideByTen = (/10)
 
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+--Implementing zipWith
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
